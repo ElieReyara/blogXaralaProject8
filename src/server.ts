b1d2ca222e7a84af5220ac1afe521759c18a5cf5
Elie REYARA <reyaraelie0@gmail.com>
2025-12-18 21:19:09 +0000

    Serveur.ts defini

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+// C'est ici qu'on assemble tout les blocs de l'application
+// On configure le serveur Express, les routes, et on demarre le serveur
+import express, { Request, Response } from "express";
+import * as dotenv from 'dotenv';
+
+import { PostController } from "./controller/PostController";
+import { PostService } from "./service/PostService";
+import { PostRepository } from "./repository/PostRepository";
+
+dotenv.config();
+const app = express();
+app.use(express.json()); // Pour qu'Express puisse lire le JSON dans les requêtes
+
+// 1. On crée une instance de PostRepository
+const postRepository = new PostRepository();
+
+// 2. On crée une instance de PostService en lui passant le PostRepository
+const postService = new PostService(postRepository);
+
+// 3. On crée une instance de PostController en lui passant le PostService
+const postController = new PostController(postService);
+
+// 4. On définit les routes et on les lie aux méthodes du PostController
+// Quand une requête GET est faite à /posts, on appelle la méthode getAllPosts du PostController
+app.get("/posts", (req: Request, res: Response) => postController.getAllPosts(req, res));
+
+// 5. On démarre le serveur sur le port défini dans les variables d'environnement ou 3000 par défaut
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
